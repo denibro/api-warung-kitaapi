5cb762a9e7c1f488222819d732ee8625535cdb3f
denibro <denisdewdew@gmail.com>
2020-10-24 22:14:03 +0700

    restfull pertama

---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/products', 'ProductController@get');
+Route::get('/products/{id}', 'ProductController@getbyid');
+
+Route::post('/product', 'ProductController@post');
+
+Route::put('/product/{id}', 'ProductController@put');
+
+Route::delete('/product/{id}', 'ProductController@delete');
